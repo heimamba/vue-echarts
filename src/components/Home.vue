e88973caea76f5fffe0432f4ed51e461fe4e3232
xxq <1633145684@qq.com>
2019-05-22 18:48:41 +0800

    swiper

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,16 +18,40 @@
     <div class="swiper-area">
       <van-swipe :autoplay="1000">
         <van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
-          <img :src="banner.imageUrl" width="80%">
+          <img v-lazy="banner.imageUrl" width="80%">
           <!-- <img v-lazy="banner.imageUrl" width="80%"> -->
         </van-swipe-item>
       </van-swipe>
+    </div>
+    <!-- easyMock -->
+    <div class="type-bar">
+      <div v-for="(cate,index) in category" :key="index">
+        <img v-lazy="cate.image" width='90%'>
+        <span>{{cate.mallCategoryName}}</span>
+      </div>
+      <!-- swiper -->
+      <!-- <swiper :options="swiperOption">
+        <swiper-slide v-for="(item,index) in recommendGoods" :key="index">
+          <div class="recommend-item">
+            <img :src="item.image" width="80%">
+            <div>{{ite.goodsName}}</div>
+            <div>${{item.price}} (${{item.mallPrice}})</div>
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div> -->
+    <swiperDefault></swiperDefault>
+    <!-- 商品推荐 -->
+    <div class="shopping-area">
+      <div class="shopping-title">商品推荐</div>
+      <div class="shopping-body"></div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import swiperDefault from '@/swiper/swiperDefault'
 export default {
   data() {
     return {
@@ -39,30 +63,35 @@ export default {
       ]
     }
   },
-  // created() {
-  //   axios({
-  //     url: 'https://www.easy-mock.com/mock/5ae2427800247c2aa1efe442/SmileVue/SmileVue/index',
-  //     method: 'get',
-  //   })
-  //   .then(response=>{
-  //     consol.log(response)
-  //     if(response.status == 200) {
-  //       this.category = response.data.data.category;
-  //     }
-  //   })
-  //   .catch((error) => {
-  //   })
-  // }
+  created() {
+    axios({
+      url: 'https://www.easy-mock.com/mock/5ae2427800247c2aa1efe442/SmileVue/SmileVue/index',
+      method: 'get',
+    })
+    .then(response=>{
+      consol.log(response)
+      if(response.status == 200) {
+        this.category = response.data.data.category;
+      }
+    })
+    .catch((error) => {
+    })
+  },
+  components: {swiper,swiperSlide,swiperDefault}
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .search-bar{
   height: 2.2rem;
   background-color: rgb(80, 46, 21);
   line-height: 2.2rem;
-}
-.search-input{
+  .location-icon{
+      padding-top: .2rem;
+      padding-left: .3rem;
+      margin: 0 auto;
+  }
+  .search-input{
       width:100%;
       height: 1.3rem;
       border-top:0px;
@@ -73,16 +102,36 @@ export default {
       color:#fff;
       text-align: center;
   }
-  .location-icon{
-      padding-top: .2rem;
-      padding-left: .3rem;
-      margin: 0 auto;
-  }
+}
   .swiper-area {
     width: 20rem;
     clear: both;
+    img {
+      height: 100px;
+    }
   }
-  .swiper-area img {
-    height: 100px;
+  .shopping-area{
+    background-color: #fff;
+    margin-top: .3rem;
+    .shopping-title{
+      border-bottom: 1px solid #eee;
+      font-size: 14px;
+      padding: .2rem;
+      color: #e5017d;
+    }
+  }
+  .type-bar {
+    background-color: #fff;
+    margin:0 .3rem .3rem .3rem;
+    border-radius: .3rem;
+    font-size:14px;
+    display: flex;
+    flex-direction:row;
+    flex-wrap:nowrap;
+    div {
+      padding: .3rem;
+      font-size: 12px;
+      text-align: center;
+    }
   }
 </style>
---
+++ src/components/swiper/swiperDefault.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <swiper>
+            <swiper-slider class="swiper-slide" v-for="(item,index) in slide" :key="index">
+                Slide {{item}}
+            </swiper-slider>
+        </swiper>
+    </div>
+</template>
+
+<script>
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+    export default {
+        data() {
+            return {
+                slide: [1, 2, 3, 4, 5, 6]
+            }
+        },
+        components:{swiper,swiperSlide}
+    }
+</script>
+
+<style lang="scss" scoped>
+    .swiper-slide {
+        height: 4rem;
+        text-align: center;
+        padding-top: 3rem;
+        border-bottom: 1px solid #ccc;
+    }
+</style>
